6a03ef0daf5f13963bb93b945e6477f19169bb53
siddhaerth <83398730+siddhaerth@users.noreply.github.com>
2022-04-08 13:50:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function startClassfication(){
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/kA4F-80yW/model.json',modelReady);
+}
+
+function modelReady(){
+    classifier.classify(gotResults);
+}
